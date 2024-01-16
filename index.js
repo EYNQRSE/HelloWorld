4055e359ef340216bda0171408060bb74cd05c33
EYNQRSE <112467899+EYNQRSE@users.noreply.github.com>
2024-01-16 15:52:37 +0800

    yes

--- index.js
+++ index.js
@@ -32,31 +32,11 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //connect to mongo
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const fs = require('fs');
-
-const credentialsPath = './X509-cert-688048128044571329.pem';
-const credentials = fs.readFileSync(credentialsPath);
-
+const credentials = './X509-cert-688048128044571329.pem'
 const client = new MongoClient('mongodb+srv://group12.7c7yswx.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
   tlsCertificateKeyFile: credentials,
   serverApi: ServerApiVersion.v1
 });
-
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db("testDB");
-    const collection = database.collection("testCol");
-    const docCount = await collection.countDocuments({});
-    console.log(docCount);
-    // perform actions using client
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
 
 const apiLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
